1982763059a8844d4ba5cb3072fa990dd24917ae
Nathan <nathan.m.g.gallet@gmail.com>
2018-04-28 10:12:28 +1200

    [client] add redux debugger

--- priv/client/src/store.js
+++ priv/client/src/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
 import rootSaga from './sagas';
 
 const configureStore = () => {
     const sagaMiddleware  = createSagaMiddleware();
+    const middleware = applyMiddleware(sagaMiddleware);
 
     const store = createStore(
         reducer,
-        applyMiddleware(sagaMiddleware)
+        compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__())
     );
     sagaMiddleware.run(rootSaga);
 
--- priv/client/webpack.prod.js
+++ priv/client/webpack.prod.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: ['babel-polyfill', './src/index.js']
+    },
     output: {
         path: path.join(__dirname, '/../static/js'),
         filename: 'bundle.js'
