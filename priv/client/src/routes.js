6a77bd40cf3a854e28a4e29ab66c23c542d874a6
Nathan <nathan.m.g.gallet@gmail.com>
2018-05-18 17:11:44 +1200

    [client] add router

--- priv/client/src/routes.js
+++ priv/client/src/routes.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+import { configureStore, history } from './store';
 import { ChatContainer } from './containers';
+
+import { ConnectedRouter } from 'react-router-redux';
+import { Route, Switch } from 'react-router-dom';
 
 const store = configureStore();
 
 // Provider wraps our root component
 const routes = (
   <Provider store={store}>
-    <div className="wrapper">
-        <ChatContainer />
-    </div>
+      <ConnectedRouter history={history}>
+          <Switch>
+              <Route path="/" component={ChatContainer} />
+          </Switch>
+      </ConnectedRouter>
   </Provider>
 );
 
