e96bb8c8fa8cd1b80e1882fdc9492fe9e0a2d076
NathanGallet <nathan.m.g.gallet@gmail.com>
2018-11-22 23:27:17 +0100

    [client] clean warning and fix front

--- priv/client/src/store.js
+++ priv/client/src/store.js
@@ -3,20 +3,20 @@ import createHistory from 'history/createBrowserHistory';
 import { routerMiddleware } from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
 
-import reducer from './reducers';
+import reducers from './reducers';
 import saga from './middleware'
 
 const history = createHistory();
 
 const configureStore = () => {
 
+    const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     const myRouterMiddleware = routerMiddleware(history);
     const sagaMiddleware     = createSagaMiddleware();
 
     const store = createStore(
-        reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__(),
-        applyMiddleware(myRouterMiddleware, sagaMiddleware)
+        reducers,
+        composeEnhancer(applyMiddleware(myRouterMiddleware, sagaMiddleware))
     );
 
     // Run the saga
@@ -25,4 +25,4 @@ const configureStore = () => {
     return store;
 }
 
-export {history, configureStore};
+export { history, configureStore };
